08ed6d26819c9c70cdd0c7cbf9b776be7cc92494
eduardo onetto <eduardo.onetto97@gmail.com>
2021-04-17 22:27:24 -0400

    Trabajar Imagenes

--- Proyecto1/App.js
+++ Proyecto1/App.js
@@ -1,48 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
+import { StyleSheet, ImageBackground, View, Text} from 'react-native';
 
 export default function App() {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
 
-  useEffect(()=> {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data)
-        setLoading(false)
-      })
-  }, [])
-
-  if (loading) {
-    return (
-    <View style= {styles.center}>
-      <Text>Cargando...</Text>
-    </View>);
-  }
   return (
     <View style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem = { ({ item }) => <Text style={styles.item}>{item.name}</Text>}
-        keyExtractor= {item => String(item.id)}
-        />
+      {/* <ActivityIndicator size="large" color="#0000ff" /> */}
+      <ImageBackground 
+      style = {styles.photo}
+      // source = {require('./assets/icon.png')}
+      source = {{ uri : 'http://placekitten.com/g/200/200'}}
+
+      >
+        <Text>LALALA</Text>
+      </ImageBackground>
     </View>
   );
-  
-
   
 }
 
 const styles = StyleSheet.create({
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 50,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
-  },
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
@@ -50,9 +27,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 22,
   },
-  center:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  photo: {
+    height: 200,
+    width: 200,
   }
 });
